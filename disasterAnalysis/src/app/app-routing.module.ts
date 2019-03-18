fa6eed584cfd304251699578108a4f1a78ca5447
Neha Navgale <nn78d@mail.umkc.edu>
2019-03-18 11:26:46 -0500

    Initial Commit
    Implemented Home page

---
+++ disasterAnalysis/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ReportComponent} from './report/report.component';
+
+const routes: Routes = [ { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  {path: 'report', component: ReportComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
